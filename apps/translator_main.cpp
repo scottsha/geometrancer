8629d6f71a315d8bd41816125769e4ed0f52b682
Shane Scott <scottshaloo@gmail.com>
2022-10-23 18:06:37 -0400

    Adding conan dependencies for helping io

---
+++ apps/translator_main.cpp
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------*/
+/*              Copyright 2020 Divergent Technologies, Inc              */
+/*----------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+#include <boost/program_options.hpp>
+
+#include "io_utilities.h"
+
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[]) {
+    std::cout << "Translate geometry files:" << std::endl;
+    return 0;
+}
+
+
--- src/utils.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by scott on 10/4/2022.
-//
-
-#include "utils.h"
